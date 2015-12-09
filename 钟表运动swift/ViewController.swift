2d772fc2f4820b3edff796ebf981e68e3764f104
wzpziyi1 <1204473090@qq.com>
2015-12-09 20:29:20 +0800

    Initial Commit

---
+++ 钟表运动swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  钟表运动swift
+//
+//  Created by 王志盼 on 15/12/9.
+//  Copyright © 2015年 王志盼. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
